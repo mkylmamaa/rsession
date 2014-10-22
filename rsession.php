d896d3ce3c747db3b25d48be46aef67f631bda0f
Marko Kylmamaa <mkylmamaa@gmail.com>
2014-10-22 19:40:52 +0000

    Initial commit

---
+++ rsession.php
@@ -0,0 +1,54 @@
+<?php
+
+// predis must be already included
+
+class RedisSessionHandler implements SessionHandlerInterface
+{
+    public $ttl = ini_get('session.gc_maxlifetime');
+    protected $db;
+    protected $prefix;
+ 
+    public function __construct(PredisClient $db, $prefix = 'PHPSESSID:') {
+        $this->db = $db;
+        $this->prefix = $prefix;
+    }
+ 
+    public function open($savePath, $sessionName) {
+        // No action necessary because connection is injected
+        // in constructor and arguments are not applicable.
+    }
+ 
+    public function close() {
+        $this->db = null;
+        unset($this->db);
+    }
+ 
+    public function read($id) {
+        $id = $this->prefix . $id;
+        $sessData = $this->db->get($id);
+        $this->db->expire($id, $this->ttl);
+        return $sessData;
+    }
+ 
+    public function write($id, $data) {
+        $id = $this->prefix . $id;
+        $this->db->set($id, $data);
+        $this->db->expire($id, $this->ttl);
+    }
+ 
+    public function destroy($id) {
+        $this->db->del($this->prefix . $id);
+    }
+ 
+    public function gc($maxLifetime) {
+        // no action necessary because using EXPIRE
+    }
+}
+
+$db_redis = new PredisClient();
+$sessHandler = new RedisSessionHandler($db_redis);
+
+session_set_save_handler($sessHandler);
+session_start();
+
+?>
